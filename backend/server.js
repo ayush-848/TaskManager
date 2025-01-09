98c170e36d1351e4997c178eba403295505ae7d7
Ayush Deb <ayushdeb848@gmail.com>
2025-01-09 12:47:54 +0530

    edit

--- backend/server.js
+++ backend/server.js
@@ -11,17 +11,16 @@ require('./src/config/connectDB')
 
 const PORT=process.env.PORT || 5000;
 
-app.get('/ping',(req,res)=>{
-  res.send("KEda re");
-})
+app.get('/ping', (req, res) => {
+  res.send('PONG');
+});
+
 app.use(bodyParser.json());
-app.use(cors())
-app.use('/auth',authRouter);
-app.use('/id',idRouter);
+app.use(cors());
+app.use('/auth', AuthRouter);
+app.use('/products', ProductRouter);
 
 
-
-
-app.listen(PORT,()=>{
-  console.log(`Server is at http://localhost:${PORT}/`)
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`)
 })
