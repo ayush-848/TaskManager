117cdd09ecd935e51ce661f8dbeae3acb6147ea1
Ayush Deb <ayushdeb848@gmail.com>
2025-01-09 13:36:30 +0530

    edit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express= require('express');
 const app=express();
-const bodyParser=require('body-parser'); // for getting the data from body
-const cors=require('cors');
+const bodyParser=require('body-parser');
 const authRouter=require('./src/routes/authRouter');
 const idRouter=require('./src/routes/idRouter')
 
@@ -11,23 +10,7 @@ require('./src/config/connectDB')
 
 const PORT=process.env.PORT || 5000;
 
-const allowedOrigins = [
-  'https://task-manager-p2.vercel.app', // Frontend domain
-];
 
-// Use CORS with specific configurations
-app.use(cors({
-  origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  credentials: true, // If you're using cookies for authentication
-}));
 
 app.get('/ping', (req, res) => {
   res.send('PONG');
