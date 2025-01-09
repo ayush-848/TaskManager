badcf1f945ffefa915cd2ef4912b7e24b423f8e9
Ayush Deb <ayushdeb848@gmail.com>
2025-01-09 12:20:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,38 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express= require('express');
+const app=express();
+const bodyParser=require('body-parser'); // for getting the data from body
+const cors=require('cors');
+const authRouter=require('./src/routes/authRouter');
+const idRouter=require('./src/routes/idRouter')
 
 require('dotenv').config();
-require('./src/config/connectDB');
-
-const authRouter = require('./src/routes/authRouter');
-const idRouter = require('./src/routes/idRouter');
-
-const PORT = process.env.PORT || 5000;
-
-// Allow CORS for specific origin
-const corsOptions = {
-  origin: 'https://task-manager-steel-one.vercel.app', // Your frontend domain
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-};
-app.options('*', cors(corsOptions)); // Enable preflight for all routes
+require('./src/config/connectDB')
 
 
-app.use(cors(corsOptions));
+const PORT=process.env.PORT || 5000;
 
+app.get('/ping',(req,res)=>{
+  res.send("KEda re");
+})
 app.use(bodyParser.json());
 
-app.get('/ping', (req, res) => {
-  res.send('KEda re');
-});
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://task-manager-steel-one.vercel.app','http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
 
-app.use('/auth', authRouter);
-app.use('/id', idRouter);
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
 
-app.listen(PORT, () => {
-  console.log(`Server is at http://localhost:${PORT}/`);
-});
+
+app.use('/auth',authRouter);
+app.use('/id',idRouter);
+
+
+
+
+app.listen(PORT,()=>{
+  console.log(`Server is at http://localhost:${PORT}/`)
+})
