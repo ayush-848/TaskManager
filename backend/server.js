bdbef68016808807e10252ba949f87a0b95a77ac
Ayush Deb <ayushdeb848@gmail.com>
2025-01-09 13:32:22 +0530

    edit

--- backend/server.js
+++ backend/server.js
@@ -11,24 +11,10 @@ require('./src/config/connectDB')
 
 const PORT=process.env.PORT || 5000;
 
-const allowedOrigins = [
-  'https://task-manager-p2.vercel.app',
-  'http://localhost:3000'
-];
-
-
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.get('/ping', (req, res) => {
   res.send('PONG');
