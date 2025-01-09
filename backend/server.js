0accfb6ecdd5fbe7547efa7a7b098af8066198f4
Ayush Deb <ayushdeb848@gmail.com>
2025-01-09 12:40:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,26 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const authRouter = require('./src/routes/authRouter');
-const idRouter = require('./src/routes/idRouter');
+const express= require('express');
+const app=express();
+const bodyParser=require('body-parser'); // for getting the data from body
+const cors=require('cors');
+const authRouter=require('./src/routes/authRouter');
+const idRouter=require('./src/routes/idRouter')
+
 require('dotenv').config();
-require('./src/config/connectDB');
-const PORT = process.env.PORT || 5000;
+require('./src/config/connectDB')
 
-// Simple CORS config that allows all origins
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
 
-app.use(bodyParser.json());
+const PORT=process.env.PORT || 5000;
 
 app.get('/ping', (req, res) => {
-  res.send("KEda re");
+  res.send('PONG');
 });
 
-app.use('/auth', authRouter);
-app.use('/id', idRouter);
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/auth', AuthRouter);
+app.use('/products', ProductRouter);
+
 
 app.listen(PORT, () => {
-  console.log(`Server is at http://localhost:${PORT}/`);
-});
+  console.log(`Server is running on ${PORT}`)
+})
